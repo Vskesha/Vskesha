6475a3b29d8ac28f5e15ebee85f9d2d8ef66af5f
Vasya <89039235+Vskesha@users.noreply.github.com>
2022-04-21 13:23:08 +0300

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
 string = input("Enter your name: ")
 age = input("Enter your age: ")
 print("Hello {}. Are you {}?.".format(string, age))
-print(f"Hello {string}. Are you {age}?")
+print(f"Hello {string}. Are you really {age}?")
